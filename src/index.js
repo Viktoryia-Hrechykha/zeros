6e05b82b7a8148f27bf34e8757c0f2a7af511189
Viktoryia_Hrechykha <viktoryia_hrechykha@epam.com>
2018-09-22 22:13:23 +0300

    answer_zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var count = 0;
+  var rest = number;
+  var i = 1;
+
+  while (rest > 1) {
+    rest = number / Math.pow(5,i);
+    count += Math.floor(rest);
+    i += 1;
+  }
+
+  return count;
+
 }
